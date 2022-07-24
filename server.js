0d504eb3334f0dcfff93cc9d1dfeef856f437e66
Mahmoud Ayman <m7m0udayman@gmail.com>
2022-07-24 17:37:27 +0200

    setup node, express, body-parser and cors, complete server.js file start up instance and declare dependencies cors and body parser and  {setup server and spin up it on port: 3000}

--- server.js
+++ server.js
@@ -2,18 +2,35 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express");
+const app = express();
 // Start up an instance of app
-
+const cors = require("cors");
+app.use(cors());
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
+// spin up server
+const port = 3000;
+
+const server = app.listen(port, () => {
+  console.log(`your local Host run on: http://localhost:${port}`);
+});
 
 // Setup Server
+app.get("/all", (req, res) => {
+  res.send(projectData);
+});
+
+// Get Route
+app.post("/add", (req, res) => {
+  projectData = req.body;
+});
--- website/app.js
+++ website/app.js
@@ -2,4 +2,9 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// API
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip="; api 
+const APIKey = ",&appid=bd10c15bae56c76cfc25506afc983f0e";
+const server = "http://localhost:3000";
